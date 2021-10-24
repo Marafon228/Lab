25931cdcd7098244fd4e10c43a79f5f5d487933a
Aaaa <d>
2021-10-24 14:02:16 +0300

    Исправил ошибки

--- Lab7/mysql_delete.php
+++ Lab7/mysql_delete.php
@@ -1,6 +1,6 @@
 <?php
 $db = mysqli_connect("localhost", "Ya", "12345", "firstbd");
-$result = mysql_query($db,"DELETE FROM 'firma' WHERE 'id' = '2'");
+$result = mysql_query($db,"DELETE FROM `firma` WHERE `id` = '2'");
 if ($result == true)
     echo "Информация из базу удалена успешно";
 else
--- Lab7/mysql_update.php
+++ Lab7/mysql_update.php
@@ -1,6 +1,6 @@
 <?php
 $db = mysqli_connect("localhost", "sdenisov", "dq7M7Z", "sdenisov_firstbd");
-$result = mysqli_query("UPDATE firma SET 'name' = 'Егор', 'lastname' = 'Егоров' WHERE 'id' = '2'");
+$result = mysqli_query($db,"UPDATE `firma` SET `name` = 'Егор', `lastname` = 'Егоров' WHERE `id` = '2'");
         if ($result == true)
             echo "Информация в базу добавлена успешно";
         else
